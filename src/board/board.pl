b5bf5b11337067f7ca3088aa187c4b5848b957ba
andreiasilva66 <up202108769@g.uporto.pt>
2023-10-11 11:12:32 +0100

    tabuleiro agora tem coordenadas

--- src/board/board.pl
+++ src/board/board.pl
@@ -9,15 +9,24 @@ createBoard([['X','X','X','X','X','X','X','X'],
 
 drawLine([]) :- print(' | ').
 
+
 drawLine([E1|E2]) :- print(' | '), print(E1),
                         drawLine(E2).
 
-
-displayBoard([]) :- print('  - - - - - - - - - - - - - - - - ').
-
-displayBoard([L1|L2]) :- print('  - - - - - - - - - - - - - - - - '),
-                        nl, drawLine(L1), nl,
-                       displayBoard(L2).
+drawLine([E1|E2], N) :- print(N), print(' | '), print(E1),
+                        drawLine(E2).
 
 
+continueDisplayBoard([],0) :- print('   - - - - - - - - - - - - - - - - ').
+
+
+
+
+continueDisplayBoard([L1|L2], N) :- print('   - - - - - - - - - - - - - - - - '),
+                        nl, drawLine(L1,N), nl,
+                        N1 is N-1,
+                       continueDisplayBoard(L2, N1).
+
+displayBoard(L) :- print(' |  A  B  C  D  E  F  G  H  |'),
+                    nl, continueDisplayBoard(L, 7).
 
